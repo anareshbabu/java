13106486257863f47d01084963cec8359652eea1
Naresh <Naresh@DESKTOP-JOF2T25>
2023-11-21 12:44:06 +0530

    First commit

---
+++ JavaTutorial/src/com/java/basics/Test.java
@@ -0,0 +1,12 @@
+package com.java.basics;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int b = Integer.parseInt("444",100);
+		//int c = Integer.pa
+		System.out.println(b);
+	}
+
+}
